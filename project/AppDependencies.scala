15e76db23e1bdc07df2c9719fbce55d52608df2c
narendravijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2026-02-12 13:17:46 +0000

    Dependencies upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,13 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "10.5.0"
-  private val hmrcMongoVersion = "2.11.0"
+  private val hmrcMongoVersion = "2.12.0"
   private val playVersion               = "play-30"
-  private val scaWrapperVersion         = "4.5.0"
+  private val scaWrapperVersion         = "4.9.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "12.27.0",
+    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "12.31.0",
     "uk.gov.hmrc"        %% "play-conditional-form-mapping-play-30"  % "3.4.0",
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "org.typelevel"      %% "cats-core"                              % "2.13.0",
