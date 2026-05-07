97bd5e839513d67dce956c88a8dccaf91a22789f
BetaDraconis <47660116+BetaDraconis@users.noreply.github.com>
2026-05-07 16:05:37 +0100

    LEPP-241: Adding placeholder files for breakdown page + message content WIP

---
+++ app/controllers/DashboardController.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.actions.{DataRetrievalAction, IdentifierAction}
+import navigation.Navigator
+import pages.{DashboardPage, WhatYouWillNeedPage}
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import services.SessionCacheService
+import viewmodels.NormalMode
+import views.html.DashboardView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class DashboardController @Inject()(identify: IdentifierAction,
+                                    getData: DataRetrievalAction,
+                                    val sessionService: SessionCacheService,
+                                    val controllerComponents: MessagesControllerComponents,
+                                    view: DashboardView,
+                                    navigator: Navigator)
+                                   (implicit val ec: ExecutionContext)
+  extends LeppBaseController(identify, getData) with I18nSupport with SessionDataHandling {
+  
+  def onPageLoad(): Action[AnyContent] = handleWithSubmissionCheck { implicit request =>
+    Future.successful(Ok(view(None, navigator.nextPage(DashboardPage, NormalMode).url)))
+  }
+
+  def onSubmit(): Action[AnyContent] = handleWithSubmissionCheck { implicit request =>
+    Future.successful(Ok(view(None, navigator.nextPage(DashboardPage, NormalMode).url)))
+  }
+}
+
