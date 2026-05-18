fa5b7b88626edde5e415f0fd7f28bf71720158f6
BetaDraconis <47660116+BetaDraconis@users.noreply.github.com>
2026-05-18 11:09:04 +0100

    LEPP-248: Remove correlation ID handler from bank details page.

--- app/controllers/WhatAreYourBankDetailsController.scala
+++ app/controllers/WhatAreYourBankDetailsController.scala
@@ -26,7 +26,6 @@ import play.api.data.Form
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import services.*
-import utils.CorrelationIdOptional
 import viewmodels.Mode
 import views.html.WhatAreYourBankDetailsView
 
@@ -36,7 +35,6 @@ import scala.concurrent.{ExecutionContext, Future}
 class WhatAreYourBankDetailsController @Inject()(identify: IdentifierAction,
                                                  getData: DataRetrievalAction,
                                                  val sessionService: SessionCacheService,
-                                                 correlationIdHandler: CorrelationIdOptional,
                                                  formProvider: WhatAreYourBankDetailsFormProvider,
                                                  view: WhatAreYourBankDetailsView,
                                                  navigator: Navigator,
@@ -46,41 +44,40 @@ class WhatAreYourBankDetailsController @Inject()(identify: IdentifierAction,
 
   private val form: Form[BankAccountDetails] = formProvider()
 
-  def onPageLoad(mode: Mode): Action[AnyContent] = handleWithLeppData { implicit req =>_ =>
-    req.userAnswers.get(WhatAreYourBankDetailsPage) match {
-      case Some(value) => Future.successful(Ok(view(form.fill(value), viewModel(mode, WhatAreYourBankDetailsPage))))
-      case None => Future.successful(Ok(view(form, viewModel(mode, WhatAreYourBankDetailsPage))))
-    }
+  def onPageLoad(mode: Mode): Action[AnyContent] = handleWithLeppData { implicit req =>
+    _ =>
+      req.userAnswers.get(WhatAreYourBankDetailsPage) match {
+        case Some(value) => Future.successful(Ok(view(form.fill(value), viewModel(mode, WhatAreYourBankDetailsPage))))
+        case None => Future.successful(Ok(view(form, viewModel(mode, WhatAreYourBankDetailsPage))))
+      }
   }
 
-  def onSubmit(mode: Mode): Action[AnyContent] = handleWithLeppData { implicit req =>_ =>
-    correlationIdHandler.handleCorrelationId(req)(implicit cid =>
-      form
-        .bindFromRequest()
-        .fold(
-          formWithErrors => Future.successful(BadRequest(
-            view(formWithErrors, viewModel(mode, WhatAreYourBankDetailsPage))
-          )),
-          answer => {
-              for {
-                updatedAnswers <- Future.fromTry(req.userAnswers.set(WhatAreYourBankDetailsPage, answer))
-                _ <- sessionService.save(updatedAnswers)
-              } yield Redirect(navigator.nextPage(WhatAreYourBankDetailsPage, mode))
-            /*barsService.checkBankAccountDetails(
-              barsRequest = answer.toBarsRequest
-            ).biSemiflatMap(
-              err => if(err.value.status == BAD_REQUEST) {
-                Future.successful(Redirect(controllers.bars.routes.BarsRequestErrorsController.onPageLoad()))
-              } else {
-                Future.successful(Redirect(controllers.bars.routes.BarsCheckFailedController.onPageLoad()))
-              },
-              _ => for {
-                updatedAnswers <- Future.fromTry(req.userAnswers.set(WhatAreYourBankDetailsPage, answer))
-                _ <- sessionService.save(updatedAnswers)
-              } yield Redirect(navigator.nextPage(WhatAreYourBankDetailsPage, mode))
-            ).merge*/
-          }
-        )
-    )
+  def onSubmit(mode: Mode): Action[AnyContent] = handleWithLeppData { implicit req => _ =>
+    form
+      .bindFromRequest()
+      .fold(
+        formWithErrors => Future.successful(BadRequest(
+          view(formWithErrors, viewModel(mode, WhatAreYourBankDetailsPage))
+        )),
+        answer => {
+          for {
+            updatedAnswers <- Future.fromTry(req.userAnswers.set(WhatAreYourBankDetailsPage, answer))
+            _ <- sessionService.save(updatedAnswers)
+          } yield Redirect(navigator.nextPage(WhatAreYourBankDetailsPage, mode))
+          /*barsService.checkBankAccountDetails(
+            barsRequest = answer.toBarsRequest
+          ).biSemiflatMap(
+            err => if(err.value.status == BAD_REQUEST) {
+              Future.successful(Redirect(controllers.bars.routes.BarsRequestErrorsController.onPageLoad()))
+            } else {
+              Future.successful(Redirect(controllers.bars.routes.BarsCheckFailedController.onPageLoad()))
+            },
+            _ => for {
+              updatedAnswers <- Future.fromTry(req.userAnswers.set(WhatAreYourBankDetailsPage, answer))
+              _ <- sessionService.save(updatedAnswers)
+            } yield Redirect(navigator.nextPage(WhatAreYourBankDetailsPage, mode))
+          ).merge*/
+        }
+      )
   }
 }
