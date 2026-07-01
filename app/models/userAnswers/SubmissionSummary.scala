55e4ed4f2f63343d49a48f924a437b58a322b586
BetaDraconis <47660116+BetaDraconis@users.noreply.github.com>
2026-07-01 15:21:25 +0100

    LEPP-366: Update submission summary model

--- app/models/userAnswers/SubmissionSummary.scala
+++ app/models/userAnswers/SubmissionSummary.scala
@@ -1,7 +1,7 @@
 package models.userAnswers
 
 case class SubmissionSummary(acceptedIds: Seq[String], notAcceptedIds: Seq[String] = Nil) {
-  val isEmpty: Boolean = this == SubmissionSummary.empty
+  val isEmpty: Boolean = acceptedIds == Nil && notAcceptedIds == Nil
   def addAccepted(leppItemId: String): SubmissionSummary = copy(acceptedIds = leppItemId +: acceptedIds)
 }
 
