2ee45c6824138c21431db60ae86a450a73ea50a3
BetaDraconis <47660116+BetaDraconis@users.noreply.github.com>
2026-07-01 15:42:22 +0100

    LEPP-366: Add Writes for SubmissionSummary model.

--- app/models/userAnswers/SubmissionSummary.scala
+++ app/models/userAnswers/SubmissionSummary.scala
@@ -1,10 +1,13 @@
 package models.userAnswers
 
+import play.api.libs.json.{Json, OWrites}
+
 case class SubmissionSummary(acceptedIds: Seq[String], notAcceptedIds: Seq[String] = Nil) {
   val isEmpty: Boolean = acceptedIds == Nil && notAcceptedIds == Nil
   def addAccepted(leppItemId: String): SubmissionSummary = copy(acceptedIds = leppItemId +: acceptedIds)
 }
 
 object SubmissionSummary {
+  given writes: OWrites[SubmissionSummary] = Json.writes[SubmissionSummary]
   val empty = SubmissionSummary(acceptedIds = Nil, notAcceptedIds = Nil)
 }
