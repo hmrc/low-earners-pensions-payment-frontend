b86e80edb4f041ec297f25f89ec6399fe3d665a1
BetaDraconis <47660116+BetaDraconis@users.noreply.github.com>
2026-07-09 11:06:57 +0100

    LEPP-249: Update ClearCacheController

--- app/controllers/ClearCacheController.scala
+++ app/controllers/ClearCacheController.scala
@@ -20,7 +20,7 @@ import controllers.actions.{DataRetrievalAction, IdentifierAction}
 import controllers.common.LeppBaseController
 import play.api.i18n.MessagesApi
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import services.SessionCacheService‘
+import services.SessionCacheService
 
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext
