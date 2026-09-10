0807c6425b490174d8c3bcc0f4f61a63a55e7b8a
narendravijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2026-09-10 13:39:38 +0100

    Updated content for cancelled payment on breakdown page

--- test/views/PaymentCalcBreakdownViewSpec.scala
+++ test/views/PaymentCalcBreakdownViewSpec.scala
@@ -140,7 +140,7 @@ class PaymentCalcBreakdownViewSpec extends SpecBase {
 
     "display cancelled history item details" in new Setup(cancelledUnderpaymentSummary, MessageKeys.SINGLE_PAST_CANCELLED_UNDER_PAYMENT, "£200", false, Some("C-id-1")) {
       view.getElementsByClass("govuk-summary-card__title-wrapper").text() mustBe "For the tax year 6 April 2024 to 5 April 2025"
-      view.getElementsByTag("h1").text mustBe "Your £200 cancelled payment calculation"
+      view.getElementsByTag("h1").text mustBe "Your additional £200 cancelled payment calculation"
     }
 
     "display available items details" in new Setup(underpaymentSummary, MessageKeys.SINGLE_UNDER_PAYMENT, "£200", false) {
